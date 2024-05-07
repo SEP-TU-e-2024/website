7e1c494b6d9e0086781ada90fe70ac3d4b5d72fd
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-05-07 20:48:59 +0200

    Updated navbar to show current page

--- frontend/src/components/navbar/MyNavbar.jsx
+++ frontend/src/components/navbar/MyNavbar.jsx
@@ -6,6 +6,7 @@ import {
     NavItem,
     NavLink 
 } from 'reactstrap'
+import { useLocation } from 'react-router-dom'
 
 /**
  * Our custom navbar component.
@@ -14,27 +15,43 @@ import {
  * @returns MyNavbar component
  */
 function MyNavbar() {
+    let location = useLocation();
+    
+    //TODO update this to work with dynamic paths (ie /leaderboard/PROBLEM_NAME/1)
+    const routeStrings = {
+        LEADERBOARD: '/leaderboard', 
+        PROBLEM_INSTANCES: '/probleminstances',
+        PROBLEM_OCCURENCES: '/problemoccurences',
+        REGISTER: '/register',
+        LOGIN: '/login',
+        HOME: '/home',
+        ROOT: '/'
+    }
+    
     
     return (
         <Navbar expand full /*fixed='top'*/ container='sm' color='dark' dark>
             <NavbarBrand href='/'>
-                <img alt='logo' src='../../../assets/react.svg' style={{height:40, width:40}} />
+                <img alt='logo' src='../../assets/react.svg' style={{height:40, width:40}} />
             </NavbarBrand>
             <Nav navbar className='me-auto' pills>
                 <NavItem>
-                    <NavLink active href='/leaderboard/'>Leaderboard</NavLink>
+                    <NavLink active={routeStrings.HOME == location.pathname || routeStrings.ROOT == location.pathname} href={routeStrings.HOME}>home</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink href='/probleminstances/'>Problem instances</NavLink>
+                    <NavLink active={routeStrings.LEADERBOARD == location.pathname} href={routeStrings.LEADERBOARD}>Leaderboard</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink href='/problemoccurences/'>Problem occurences</NavLink>
+                    <NavLink active={routeStrings.PROBLEM_INSTANCES == location.pathname} href={routeStrings.PROBLEM_INSTANCES}>Problem instances</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink href='/register/'>Registration</NavLink>
+                    <NavLink active={routeStrings.PROBLEM_OCCURENCES == location.pathname} href={routeStrings.PROBLEM_OCCURENCES}>Problem occurences</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink href='/login/'>Login</NavLink>
+                    <NavLink active={routeStrings.REGISTER == location.pathname} href={routeStrings.REGISTER}>Registration</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink active={routeStrings.LOGIN == location.pathname} href={routeStrings.LOGIN}>Login</NavLink>
                 </NavItem>
             </Nav>
         </Navbar>
