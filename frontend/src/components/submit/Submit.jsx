eacb8517da07a2a425ebad6916f20bf9725937b2
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-05-07 18:27:18 +0200

    Fixed yarn and packages

--- frontend/src/components/submit/Submit.jsx
+++ frontend/src/components/submit/Submit.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import './Submit.css';
-import { FileUploader } from "react-drag-drop-files";
+// import React, { useState } from 'react';
+// import './Submit.css';
+// import { FileUploader } from "react-drag-drop-files";
 
-const fileTypes = ["ZIP"];
+// const fileTypes = ["ZIP"];
 
-function Submit() {
-  const [file, setFile] = useState(null);
+// function Submit() {
+//   const [file, setFile] = useState(null);
 
-  const handleChange = (file) => {
-    console.log("File selected:", file);
-    setFile(file);
-  };
+//   const handleChange = (file) => {
+//     console.log("File selected:", file);
+//     setFile(file);
+//   };
 
-  console.log("File state:", file);
+//   console.log("File state:", file);
 
-  return (
-    <div>
-      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
-      {file && <img src={file.preview} alt="Uploaded file" />}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+//       {file && <img src={file.preview} alt="Uploaded file" />}
+//     </div>
+//   );
+// }
 
-export default Submit;
+// export default Submit;
