d695f6c9832292d323ff47de5746ac664069a601
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-05-08 11:15:56 +0200

    made the expand navbar responsive

--- frontend/src/components/navbar/MyNavbar.jsx
+++ frontend/src/components/navbar/MyNavbar.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Navbar,
     NavbarBrand,
     Nav,
     NavItem,
     NavLink,
-    Container 
+    NavbarToggler,
+    Collapse
 } from 'reactstrap'
 import { useLocation } from 'react-router-dom'
 
@@ -17,6 +18,9 @@ import { useLocation } from 'react-router-dom'
  */
 function MyNavbar() {
     let location = useLocation();
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const toggle = () => setIsOpen(!isOpen); //toggle the isOpen state variable
     
     //TODO update this to work with dynamic paths (ie /leaderboard/PROBLEM_NAME/1)
     const routeStrings = {
@@ -31,36 +35,40 @@ function MyNavbar() {
     
     
     return (
-        <Navbar expand full /*fixed='top'*/ container='sm' color='dark' dark>
+        <Navbar expand={'md'} /*fixed='top'*/ container='md' color='dark' dark>
             <NavbarBrand href='/'>
                 <img alt='logo' src='/src/assets/react.svg' style={{height:40, width:40}} />
             </NavbarBrand>
-            
-            {/* Navbar left aligned items */}
-            <Nav navbar className='me-auto' pills>
-                <NavItem>
-                    <NavLink active={routeStrings.HOME == location.pathname || routeStrings.ROOT == location.pathname} href={routeStrings.HOME}>home</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink active={routeStrings.LEADERBOARD == location.pathname} href={routeStrings.LEADERBOARD}>Leaderboard</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink active={routeStrings.PROBLEM_INSTANCES == location.pathname} href={routeStrings.PROBLEM_INSTANCES}>Problem instances</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink active={routeStrings.PROBLEM_OCCURENCES == location.pathname} href={routeStrings.PROBLEM_OCCURENCES}>Problem occurences</NavLink>
-                </NavItem>
-            </Nav>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
                 
-            {/* Navbar right aligned items */}
-            <Nav navbar className='ms-auto' pills>
-                <NavItem>
-                    <NavLink active={routeStrings.REGISTER == location.pathname} href={routeStrings.REGISTER}>Registration</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink active={routeStrings.LOGIN == location.pathname} href={routeStrings.LOGIN}>Login</NavLink>
-                </NavItem>
-            </Nav>
+                {/* Navbar left aligned items */}
+                <Nav navbar className='me-auto' pills>
+                    <NavItem>
+                        <NavLink active={routeStrings.HOME == location.pathname || routeStrings.ROOT == location.pathname} href={routeStrings.HOME}>home</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink active={routeStrings.LEADERBOARD == location.pathname} href={routeStrings.LEADERBOARD}>Leaderboard</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink active={routeStrings.PROBLEM_INSTANCES == location.pathname} href={routeStrings.PROBLEM_INSTANCES}>Problem instances</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink active={routeStrings.PROBLEM_OCCURENCES == location.pathname} href={routeStrings.PROBLEM_OCCURENCES}>Problem occurences</NavLink>
+                    </NavItem>
+                </Nav>
+                    
+                {/* Navbar right aligned items */}
+                <Nav navbar className='ms-auto' pills>
+                    <NavItem>
+                        <NavLink active={routeStrings.REGISTER == location.pathname} href={routeStrings.REGISTER}>Registration</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink active={routeStrings.LOGIN == location.pathname} href={routeStrings.LOGIN}>Login</NavLink>
+                    </NavItem>
+                </Nav>
+                
+            </Collapse>
         </Navbar>
     )
 }
