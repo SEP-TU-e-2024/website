61bbf4e2834d2513928c95a1c2d4e6ac4dccc3a4
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-05-08 18:47:24 +0200

    Formatted code according to ruff settings

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
