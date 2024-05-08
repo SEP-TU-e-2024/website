6605c1ece0ce9d3f1e329d32c3c672c4b1929b31
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-05-08 13:35:18 +0200

    Made a skeleton react component for the Prob occurence overview

---
+++ frontend/src/components/problemOccurenceOverview/ProblemOccurenceOverviewPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Container } from 'reactstrap'
+
+function ProblemOccurenceOverviewPage() {
+  return (
+    <Container>
+        <h1>This is a problem occurence overview page</h1>
+    </Container>
+  )
+};
+
+export default ProblemOccurenceOverviewPage;
