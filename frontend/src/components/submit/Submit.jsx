e94c66f50c1a137fdcedd5246c20e08e17ecc3c4
Cristian Fildan <c.fildan@student.tue.nl>
2024-05-08 15:26:17 +0200

    finished checks for file name/size/type

--- frontend/src/components/submit/Submit.jsx
+++ frontend/src/components/submit/Submit.jsx
@@ -7,6 +7,7 @@ function Submit() {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
+    //
     setFile(event.target.files[0]);
   };
   const navigate = useNavigate();
@@ -14,22 +15,45 @@ function Submit() {
 
   const uploadHandler = () => {
     if (file) {
+
+      // Check file type
+      if (!['application/zip', 'application/x-rar-compressed', 'application/x-7z-compressed'].includes(file.type)) {
+        console.error('Invalid file type.');
+        alert("Invalid file type. Please upload a .zip, .rar, or .7zip file.")
+        return;
+      }
+   
+      // Check file name length
+      if (file.name.length > 50) {
+        console.error('File name exceeds 50 characters.');
+        alert("File name exceeds 50 characters.")
+        return;
+      }
+      // Check file size
+      if (file.size > 50 * 1024 * 1024) { // 50 MB in bytes
+        console.error('File size exceeds 50MB.');
+        alert("File size exceeds 50MB.")
+        return;
+      }
+      
+  
       const formData = new FormData();
       formData.append('file', file);
-
+  
       api.post('submit/upload_file/', formData)
         .then(response => {
           console.log(response.data);
           // Handle success response
+          // alert("Successfully submitted file!");
+          // navigate('/home');
         })
         .catch(error => {
           console.error('Error uploading file:', error);
           // Handle error response
         });
-        // alert("Successfully submitted file!");
-        // navigate('/home');
-      }
+    }
   };
+  
 
   return (
     <div className="Submit">
@@ -39,6 +63,7 @@ function Submit() {
         accept=".zip,.rar,.7zip"
         onChange={handleFileChange}
       />
+      {/* <></> */}
       <button onClick={uploadHandler}>Upload File</button>
     </div>
   );
