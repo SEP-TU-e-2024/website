58f859488fde7da7b58713eb5548b1cd17973160
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-05-23 12:39:58 +0200

    reapplied strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap-icons/font/bootstrap-icons.scss'
 import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+  <React.StrictMode>
     <RouterProvider router={router} />,
-  //</React.StrictMode>
+  </React.StrictMode>
 )
