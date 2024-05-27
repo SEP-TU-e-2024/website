e029d3ccd59b0c84f6fb1d2de52f83743a8b5d40
Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>
2024-05-27 19:15:20 +0200

    Registration improvements (#36)
    * Fixed bug with logging in
    
    * Updated login and registration
    
    * Fixed code quality checks
    
    * Comments fixes/removal
    
    * Addressed comments
    
    ---------
    
    Co-authored-by: ronalddenouden <104371933+ronalddenouden@users.noreply.github.com>

--- backend/api/views/problem_view.py
+++ backend/api/views/problem_view.py
@@ -3,7 +3,6 @@
 from django.db.models import Count
 from django.http import HttpResponseNotFound, JsonResponse
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
 from ..models import SpecifiedProblem
@@ -11,18 +10,17 @@ from ..serializers import SpecifiedProblemSerializer
 
 
 class Problems(APIView):
-    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         """
         Serializes specified problems for the list of problem occurences
         """
-        
+
         # Joining tables and adding field
         if 'POId' in request.data:
             problems = SpecifiedProblem.objects.all().prefetch_related("category").prefetch_related("evaluation_settings").annotate(submission_count=Count('submission')).filter(id=request.data['POId'])
             if len(problems) == 0:
-                #when there is no problem with the requested id, return a 404 response
+                # When there is no problem with the requested id, return a 404 response
                 return HttpResponseNotFound()
         else:
             problems = SpecifiedProblem.objects.all().prefetch_related("category").annotate(submission_count=Count('submission'))
--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,69 +1,90 @@
 import { useState, useEffect, useContext } from 'react'
 import axios from 'axios';
-import './Form.css';
+import './Form.scss';
 import { useNavigate } from "react-router-dom";
 import AuthContext from '../../context/AuthContext';
-
+import { Container, Row, Col, TabContent, TabPane } from 'reactstrap'
 
 function LoginForm() {
-
+    const [passwordVisiblity, setPasswordVisibility] = useState(false); 
     const navigate = useNavigate();
     let {login_user} = useContext(AuthContext)
     let {send_email_login} = useContext(AuthContext)
 
-    const handleRegister = (event) => {
-        navigate("/register")
+    const handleForm = (event) => {
+        setPasswordVisibility(!passwordVisiblity)
     };
 
     const PasswordForm = () => {
         return (
+            <>
+            <h5 className='text_banner'>
+                Please enter your credentials down below, you will receive a session afterwards.
+            </h5>
             <div className='form_container'>
                 <form onSubmit={login_user} method='post'>
-                    <div>
+                    
+                    <div className='field_container'>
+                        <p>Email</p>
                         <input
                             name="email"
                             type="text"
-                            placeholder={"Email"}
                             required/>
                     </div>
-                    <div>
+                    <div className='field_container'>
+                        <p>Password</p>
                         <input
                             name="password"
                             type="password"
-                            placeholder={"Password"}
                             required
                         />
                     </div>
                     <button type="submit">Login</button>
                 </form>
-                <p onClick={handleRegister}> Or register here! </p>
+                <p onClick={handleForm}> Or login with email </p>
             </div>
+            </>
         )
     }
 
     const EmailForm = () => {
         return (
+            <>
+            <h5 className='text_banner'>
+                Please enter your email. You will promptly receive an email with a link that grants you a session.
+            </h5>
             <div className='form_container'>
                 <form onSubmit={send_email_login} method='post'>
-                    <div>
+                    <div className='field_container'>
+                        <p>Email</p>
                         <input
                             name="email"
                             type="text"
-                            placeholder={"Email"}
                             required/>
                     </div>
                     <button type="submit"> Send email </button>
                 </form>
+                <p onClick={handleForm}> Or login with password </p>
             </div>
+            </>
         )
     }
 
     return (
         <div className='login_page'>
-            <h2> Email login option</h2>
-            <EmailForm/>
-            <h2> Password login option</h2>
-            <PasswordForm/>
+            <Container fluid className="bg-primary mt-4">
+                <Row className="justify-content-center">
+                    <Col className='text-light text-center py-5' xs="8">
+                        <h1 className="fw-bold">Login</h1>
+                    </Col>
+                </Row>
+            </Container>
+            {passwordVisiblity ? (
+                <PasswordForm/>
+            ) :
+            (
+                <EmailForm/>
+            )}
         </div>
     );
 };
