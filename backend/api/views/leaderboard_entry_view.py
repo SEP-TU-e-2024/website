f5b8f1385618efb4c32ca6a9eaddbfeb7354e2c8
marnikdenouden <marnikdenouden@gmail.com>
2024-05-28 10:49:51 +0200

    Add comments for style check
    Add comments to leaderboard_entry_view.py and leaderboard_view.py

--- backend/api/views/leaderboard_entry_view.py
+++ backend/api/views/leaderboard_entry_view.py
@@ -8,14 +8,21 @@ from ..models import Submission
 
 
 class LeaderboardEntryView(APIView):
+    """Leaderboard entry view as end point to get data from"""
 
     def get(self, request, submission_id:int) -> JsonResponse:
+        """Get request to retrieve a leaderboard entry for a submission id."""
+
+        # Get the submission from the id
         submission = Submission.objects.all().filter(id=submission_id)
 
+        # Robustness check to see if submission exists
         if not submission.exists():
-            # f"Submission with id {submission_id} was not found"
-            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error':f"Submission with id {submission_id} was not found"},
+                             status=status.HTTP_400_BAD_REQUEST)
 
+        # Create a leaderboard entry for the submission
         serializer = LeaderboardEntrySerializer(LeaderboardEntry(submission.get()))
         
+        # Send JsonResponse with the serialized data of the leaderboard entry
         return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
--- backend/api/views/leaderboard_view.py
+++ backend/api/views/leaderboard_view.py
@@ -8,14 +8,21 @@ from ..models import SpecifiedProblem
 
 
 class LeaderboardView(APIView):
+    """Leaderboard view as end point to get data from"""
     
     def get(self, request, problem_id:int) -> JsonResponse:
+        """Get request to retrieve a leaderboard for a problem id."""
+        
+        # Get the problem from the id
         problem = SpecifiedProblem.objects.all().filter(id=problem_id)
 
+        # Robustness check to see if problem exists
         if not problem.exists():
-            # f"Problem with id {problem_id} was not found"
-            return Response({}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error':f"Problem with id {problem_id} was not found"},
+                             status=status.HTTP_404_NOT_FOUND)
 
+        # Create a leaderboard for the problem
         serializer = LeaderboardSerializer(Leaderboard(problem.get()))
 
+        # Send JsonResponse with the serialized data of the leaderboard
         return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
