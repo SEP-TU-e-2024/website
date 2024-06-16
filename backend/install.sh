619518bb79e576f9065878aa158d68249c6ed7a7
TPGamesNL <pet.teun.03@gmail.com>
2024-06-16 13:36:56 +0200

    Ofc it wouldnt work first time what was i thinking

--- backend/install.sh
+++ backend/install.sh
@@ -5,16 +5,14 @@ set -e
 # Install Python and dependencies
 echo "-- Installing Python and dependencies"
 apt-get update
-apt-get install -y software-properties-common wget
+apt-get install -y software-properties-common wget pkg-config default-libmysqlclient-dev build-essential
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
-apt-get install -y python3.11 python3.11-venv
+apt-get install -y python3.11 python3.11-dev python3.11-venv
 wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
 python3.11 get-pip.py
 
 python3.11 -m pip install -r requirements.txt
-
-apt-get install -y pkg-config default-libmysqlclient-dev build-essential
 
 echo "-- Loading Django migrations & fixture"
 python3.11 manage.py migrate
