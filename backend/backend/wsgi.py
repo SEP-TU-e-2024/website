1d3fb18416a87d892e4d3a71d29df7b678c78611
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-06-17 19:52:05 +0200

    Modified wsgi.py again for testing

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -11,11 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from backend.evaluator import initiate_protocol
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
+
+from backend.evaluator import initiate_protocol  # noqa: E402
 
 # Start the Backend <-> Judge protocol
 initiate_protocol()
