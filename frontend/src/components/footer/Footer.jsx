db0bab9806fedab3e9a9b89c32313b4f5c3ba707
Thijs Egberts <t.t.a.egberts@student.tue.nl>
2024-06-18 16:01:23 +0200

    removed and added comments

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,17 +3,18 @@ import "./Footer.scss";
 import { Container, Row, Col } from "reactstrap";
 import { Navigate, useNavigate } from "react-router-dom";
 
+/**
+ * Footer component with a link to the about page.
+ * 
+ * @returns Footer component
+ */
 function Footer() {
   const navigate = useNavigate();
   
   return (
     <footer>
       <Container className="justify-content-end">
-        {/* <Row className="justify-content-end">
-          <Col className=""> */}
-            <a role="button" onClick={() => navigate("/about")}><i className="bi-info-circle" /> About</a>
-          {/* </Col>
-        </Row> */}
+        <a role="button" onClick={() => navigate("/about")}><i className="bi-info-circle" /> About</a>
       </Container>
     </footer>
   )
