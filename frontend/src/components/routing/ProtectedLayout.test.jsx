e8a3dc1edbc4a9e836bcbb143f96b11fa781ec94
Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>
2024-06-24 16:12:22 +0200

    Unittests (#216)
    * Removed node_modules folder in the website directory
    
    * Set up first unit tests (navbar loging & logo)
    
    * Unit tests for UnprotectedLayout and collapsing navbar
    
    * Added code coverage check
    
    * Unit tests for logo to homebar navigation
    
    * Generated code coverage files
    
    * Testing HomePage first edition
    
    * Try to fix npm install to yarn add
    
    * Added HomePage and App tests
    
    * Added coverage files to gitignore
    
    * Added test cases for Submit
    
    * Added tests for ProblemOccurrenceOverviewPage
    
    * Some small fixes
    
    * Refactoring
    
    * Fixed navigation error in UnprotectedLayout.test.jsx
    
    * Merged main into this branch. Fixed unit test installation
    
    * Fix unit test library install (hopefully)
    
    * Fixed alert bug
    
    * Merge AlertProvider fix
    
    * Rendering to test utils
    
    * Fixed unit tests
    
    ---------
    
    Co-authored-by: Booksh <e.t.s.zandbelt@student.tue.nl>

---
+++ frontend/src/components/routing/ProtectedLayout.test.jsx
@@ -0,0 +1,38 @@
+import { screen } from "@testing-library/react";
+import { beforeEach, describe, expect, vi } from "vitest";
+import ProtectedLayout from "./ProtectedLayout";
+import userEvent from "@testing-library/user-event";
+import { mockMemberContextData, renderWithRouter } from "../testing_utils/TestingUtils";
+
+describe("Protected layout", () => {
+    beforeEach(() => {
+        // Reset window.location to the homepage to avoid leaking state
+        window.location.assign("/");
+    });
+
+    it("should show a member the logout button", () => {
+        renderWithRouter(true, ProtectedLayout);
+        // Find logout button
+        const logoutButton = screen.getByText("Logout");
+        expect(logoutButton).toBeInTheDocument();
+    });   
+
+    it("should call logout function when logout button is clicked", async () => {
+        renderWithRouter(true, ProtectedLayout);
+        // Find logout button
+        const logoutButton = screen.getByText("Logout");
+        // Simulate click of logout button
+        await userEvent.click(logoutButton);
+        expect(mockMemberContextData.logout_user).toHaveBeenCalledOnce();
+    });
+
+    it("should navigate a guest to login", () => {
+        // Should not be at login yet
+        expect(window.location.pathname).not.toContain("/login");
+        renderWithRouter(false, ProtectedLayout);
+        // Should have redirected a guest to the login page
+        expect(window.location.pathname).toContain("/login");
+    });
+    
+    
+});
