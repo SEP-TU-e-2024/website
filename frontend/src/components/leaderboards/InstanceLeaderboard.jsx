b80aa3dde0b014924e81e700c6753233b54131aa
Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>
2024-06-26 09:52:42 +0200

    Leaderboard unittests (#218)
    * Some initial tests
    
    * Almost complete coverage on instance leaderboard and normal leaderboard
    
    * Test for aggregated leaderboard, excluding blob download
    
    * Adjustments based on feedback
    
    * Changes based on feedback

--- frontend/src/components/leaderboards/InstanceLeaderboard.jsx
+++ frontend/src/components/leaderboards/InstanceLeaderboard.jsx
@@ -19,6 +19,7 @@ function InstanceLeaderboard({problemData, leaderboardData, instance}) {
   const columns = createColumns(problemData, instance);
   // Copy is to we only affect this leaderboard when ranking the instance entries.
   const instanceLeaderboardData = [...leaderboardData];
+
   rankInstanceEntries(problemData, instanceLeaderboardData, instance);
   return <Leaderboard problemData={problemData} columns={columns} leaderboardData={instanceLeaderboardData} LeaderboardRow={LeaderboardRow}/>
 }
@@ -29,7 +30,7 @@ function InstanceLeaderboard({problemData, leaderboardData, instance}) {
  * @param {JSON} entry a single entry in the leaderboard
  * @returns 
  */
-function LeaderboardRow({columns, entry}) {
+export function LeaderboardRow({columns, entry}) {
   return (
     <>
       <tr className="view">
@@ -50,6 +51,9 @@ function LeaderboardRow({columns, entry}) {
  */
 function createColumns(problem, instance) {
   let columns = [];
+  if (problem.length === 0) {
+    return columns;
+  }
 
   columns.push(new LeaderboardColumn("#", 
     (entry) => { return entry.instance_entries[instance].rank === 0 ? 
@@ -70,31 +74,8 @@ function createColumns(problem, instance) {
       columns.push(new MetricColumn(metric, (entry) => { return entry.instance_entries[instance].results; }))
     }
   });
-
-  // columns.push(new LeaderboardColumn("Download Solution", 
-  //   (entry) => { return <div className="download-cell"><i role="button" onClick={handleDownloadSolutionsClick} className="bi-download" /></div> },
-  //   <div className="download-cell">Download Solutions</div>
-  // ));
-  // columns.push(new LeaderboardColumn("Download Scores", 
-  //   (entry) => { return <div className="download-cell"><i role="button" onClick={handleDownloadScoresClick} className="bi-download" /></div> },
-  //   <div className="download-cell">Download Scores</div>
-  // ));
-
+  
   return columns;
-}
-
-// Download solutions handler
-function handleDownloadSolutionsClick(e) {
-  e.stopPropagation();
-  // TODO: Change to showAlert
-  alert("download solutions");
-}
-
-// Download scores handler
-function handleDownloadScoresClick(e) {
-  e.stopPropagation();
-  // TODO: Change to showAlert
-  alert("download scores");
 }
 
 /**
@@ -104,8 +85,12 @@ function handleDownloadScoresClick(e) {
  * @param {int} rowLimit the limit to the amount of displayed rows 
  * @param {bool} showPagination whether to show a pagination component (not implemented yet)
  */
-function rankInstanceEntries(problemData, leaderboardData, instance) {
+export function rankInstanceEntries(problemData, leaderboardData, instance) {
   const rankableInstanceEntries = [];
+
+  if (problemData.length === 0) {
+    return leaderboardData;
+  }
   
   for (let entry of leaderboardData) {
     let instanceEntry = entry.instance_entries[instance];
@@ -137,4 +122,4 @@ function rankInstanceEntries(problemData, leaderboardData, instance) {
   })
 }
 
-export default InstanceLeaderboard;
+export default InstanceLeaderboard; 
---
+++ frontend/src/components/leaderboards/InstanceLeaderboard.test.jsx
@@ -0,0 +1,162 @@
+import { afterEach, beforeEach, describe, expect, vi } from "vitest";
+import { screen, waitFor, render } from "@testing-library/react";
+import { mockLeaderboardData, mockColumns, mockProblemDataLeaderboard, renderWithRouter } from "../testing_utils/TestingUtils";
+import InstanceLeaderBoard from "./InstanceLeaderBoard";
+import { LeaderboardRow, rankInstanceEntries } from "./InstanceLeaderBoard";
+
+describe("Leaderboard", () => {
+    beforeEach(() => {
+        // Reset window.location to the homepage to avoid leaking state
+        window.location.assign('/');
+        // Reset the mock object to avoid leaking state
+        vi.resetAllMocks();
+    });
+
+    afterEach(() => {
+        // Restore the mock object to its original state after each test to avoid leaking state    
+        vi.restoreAllMocks();
+    });
+
+    it("should display 'Error: no column names found' if no data is given", async () => {
+        // Render the InstanceLeaderBoard component wrapped in BrowserRouter
+        renderWithRouter(true, () => (
+            <InstanceLeaderBoard 
+                problemData={[]}
+                leaderboardData={mockLeaderboardData}
+                instance={0}
+            />)
+        );
+
+        // Wait for the useEffect and fetchData to complete
+        await waitFor(async () => {
+            // Check whether the correct text is displayed
+            expect(screen.getByText('Error: no column names found')).toBeInTheDocument();
+        });
+    });
+
+    it("should display 'No entries found' if no data is given", async () => {
+        // Render the InstanceLeaderBoard component wrapped in BrowserRouter
+        renderWithRouter(true, () => (
+            <InstanceLeaderBoard 
+                problemData={mockProblemDataLeaderboard}
+                leaderboardData={[]}
+                instance={0}
+            />)
+        );
+
+        // Wait for the useEffect and fetchData to complete
+        await waitFor(async () => {
+            // Check whether the correct text is displayed
+            expect(screen.getByText('No entries found')).toBeInTheDocument();
+        });
+    });
+
+    // Simple good case
+    it("should display mock entries and columns", async () => {
+        // Render the InstanceLeaderBoard component wrapped in BrowserRouter
+        renderWithRouter(true, () => (
+            <InstanceLeaderBoard 
+                problemData={mockProblemDataLeaderboard}
+                leaderboardData={mockLeaderboardData}
+                instance={0}
+            />)
+        );
+
+        // Wait for the useEffect and fetchData to complete
+        await waitFor(async () => {
+            // Check whether the correct text is displayed
+            expect(screen.getByText('Submitted by')).toBeInTheDocument();
+            expect(screen.getByText('Submission name')).toBeInTheDocument();
+            expect(screen.getByText('Submission date')).toBeInTheDocument();
+
+            // Dynamic data
+            const janeSmithInstances = screen.getAllByText('Jane Smith');
+            expect(janeSmithInstances).toHaveLength(2);
+            expect(screen.getByText('John Doe')).toBeInTheDocument();
+            expect(screen.getByText('example submission one')).toBeInTheDocument();
+            expect(screen.getByText('example submission two')).toBeInTheDocument();
+            expect(screen.getByText('example submission three')).toBeInTheDocument();
+        });
+    });
+
+    // Conditional render test
+    it("Leaderboard shows result for instance 0", async () => {
+        // Render the InstanceLeaderBoard component wrapped in BrowserRouter
+        renderWithRouter(true, () => (
+            <InstanceLeaderBoard 
+                problemData={mockProblemDataLeaderboard}
+                leaderboardData={mockLeaderboardData}
+                instance={0}
+            />)
+        );
+
+        // Wait for the useEffect and fetchData to complete
+        await waitFor(async () => {
+            // Check whether the correct text is displayed
+            expect(screen.getByText('14')).toBeInTheDocument();
+            expect(screen.queryByText('13')).not.toBeInTheDocument();
+        });
+    });
+
+    // Conditional render test
+    it("Leaderboard shows result for instance 1", async () => {
+        // Render the InstanceLeaderBoard component wrapped in BrowserRouter
+        renderWithRouter(true, () => (
+            <InstanceLeaderBoard 
+                problemData={mockProblemDataLeaderboard}
+                leaderboardData={mockLeaderboardData}
+                instance={1}
+            />)
+        );
+
+        // Wait for the useEffect and fetchData to complete
+        await waitFor(async () => {
+            // Check whether the correct text is displayed
+            expect(screen.getByText('13')).toBeInTheDocument();
+            expect(screen.queryByText('14')).not.toBeInTheDocument();
+        });
+    });
+
+    // Row test
+    it("row test", async () => {
+        // Render the LeaderboardRow component wrapped in BrowserRouter
+        render(
+            <LeaderboardRow 
+                columns={mockColumns()}
+                entry={mockLeaderboardData[0]}
+            />
+        );
+
+        // Wait for the useEffect and fetchData to complete
+        await waitFor(async () => {
+            // Check whether the correct text is displayed
+            expect(screen.queryByText('John Doe')).toBeInTheDocument();
+            expect(screen.queryByText('Jane Smith')).not.toBeInTheDocument();
+        });
+    });
+
+    it("Sorting from high to low", async () => {
+
+        rankInstanceEntries(mockProblemDataLeaderboard, mockLeaderboardData, 0);
+        expect(mockLeaderboardData[0].instance_entries[0].rank).toBe(1); // Highest score
+        expect(mockLeaderboardData[1].instance_entries[0].rank).toBe(2); // Second highest score
+        expect(mockLeaderboardData[2].instance_entries[0].rank).toBe(3); // Lowest score entry
+    });
+
+    it("Sorting from low to high", async () => {
+        const dataCopy = [...mockLeaderboardData]
+        mockProblemDataLeaderboard.scoring_metric["order"] = 0;
+        rankInstanceEntries(mockProblemDataLeaderboard, mockLeaderboardData, 0);
+
+        expect(dataCopy[0].instance_entries[0].rank).toBe(3); // Highest score
+        expect(dataCopy[1].instance_entries[0].rank).toBe(2); // Second highest score
+        expect(dataCopy[2].instance_entries[0].rank).toBe(1); // Lowest entry
+    });
+
+    it("Sorting with misformatted data", async () => {
+        rankInstanceEntries(mockProblemDataLeaderboard, mockLeaderboardData, 1);
+        expect(mockLeaderboardData[0].instance_entries[1].rank).toBe(1); // Highest score
+        expect(mockLeaderboardData[1].instance_entries[1].rank).toBe(2); // Second highest score
+        expect(mockLeaderboardData[2].instance_entries[1].rank).toBe(0); // No score entry
+    });
+});
