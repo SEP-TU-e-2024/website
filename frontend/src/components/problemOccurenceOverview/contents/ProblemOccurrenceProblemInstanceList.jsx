df639cffcca0755759bf715e96dacb8622b24637
ThijsEgberts <104772243+ThijsEgberts@users.noreply.github.com>
2024-06-28 12:05:34 +0200

    Alternative problem instance leaderbords (#228)
    * Changed the instance leaderboard selector to buttons
    
    * Styling of the menu
    
    * fixed dumdum spelling mistake
    
    * Added length 0 check
    
    * Update unit tests
    
    * bomba
    
    ---------
    
    Co-authored-by: Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>

--- frontend/src/components/problemOccurenceOverview/contents/ProblemOccurrenceProblemInstanceList.jsx
+++ frontend/src/components/problemOccurenceOverview/contents/ProblemOccurrenceProblemInstanceList.jsx
@@ -1,29 +1,60 @@
 import React, {useState} from "react"
 import { Container, Row, Col } from "reactstrap";
 import InstanceLeaderboard from "../../leaderboards/InstanceLeaderboard";
+
+import "./ProblemOccurrenceProblemInstanceList.scss";
+
 /**
  * A component for listing the problem instances included in a problem occurence.
  */
 function ProblemOccurrenceProblemInstanceList({problemData, leaderboardData}) {
-  const [selectedInstance, setSelectedInstance] = useState(0);
-
-  const handleInstanceChange = (event) => {
-    setSelectedInstance(event.target.value);
-  };
 
   return (
     <Container className="ps-0 pt-2">
         <Row className="">
           <Col>
-            <select onChange={handleInstanceChange} value={selectedInstance}>
-              {problemData.benchmark_instances.map((instance, index) => (
-                <option key={index} value={index}> {instance.name} </option>
-              ))}
-            </select>
-            <InstanceLeaderboard instance={selectedInstance} leaderboardData={leaderboardData} problemData={problemData}/>
+            <Container fluid className="justify-content-center">
+              <table id="problem-instance-table">
+                <thead>
+                  <tr>
+                    <th>Problem instance name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {problemData.benchmark_instances.length == 0 ? 
+                  <tr><td align="center" className="text-danger">No entries found</td></tr>
+                  :
+                  problemData.benchmark_instances.map((instance, index) => (
+                    // The console cries about this being a div in a table but just ignore that.
+                    <InstanceLeaderboardInstanceEntry instance={instance} instanceIndex={index} leaderboardData={leaderboardData} problemData={problemData}/>
+                    ))}
+                </tbody>
+              </table>
+            </Container>
           </Col>
         </Row>
     </Container>
+  )
+};
+
+function InstanceLeaderboardInstanceEntry({instance, instanceIndex, leaderboardData, problemData}) {
+  const [leaderboardOpen, setLeaderboardOpen] = useState(false);
+  
+  const handleLeaderboardToggle = (event) => {
+    setLeaderboardOpen(!leaderboardOpen);
+  };
+  
+  return (
+    <>
+      <tr className="instance-button" onClick={handleLeaderboardToggle} role="button">
+        <td>{instance.name}</td>
+      </tr>
+      <tr className="instance-container" style={leaderboardOpen ? {display:"table-row"} : {display:"none"}}>
+        <td>
+          <InstanceLeaderboard instance={instanceIndex} leaderboardData={leaderboardData} problemData={problemData} />
+        </td>
+      </tr>
+    </>
   )
 };
 
--- frontend/src/components/problemOccurenceOverview/contents/ProblemOccurrenceProblemInstanceList.test.jsx
+++ frontend/src/components/problemOccurenceOverview/contents/ProblemOccurrenceProblemInstanceList.test.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
-import { expect } from 'vitest';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { expect, it } from 'vitest';
 import ProblemOccurrenceProblemInstanceList from './ProblemOccurrenceProblemInstanceList';
 import { mockProblemDataLeaderboard, mockLeaderboardData, renderWithRouter } from '../../testing_utils/TestingUtils';
 
 
 describe('Problem occurrence instance list', () => {    
+    beforeEach(() => {
+        // Reset window.location to the homepage to avoid leaking state
+        window.location.assign('/');
+        // Reset the mock object to avoid leaking state
+        vi.resetAllMocks();
+    });
 
+    afterEach(() => {
+        // Restore the mock object to its original state after each test to avoid leaking state    
+        vi.restoreAllMocks();
+    });
+    
+    it("should display 'No entries found' if no data is given", async () => {
+        //transform the mock data to not have any problem instances
+        let noProblemInstancesMockProblemDataLeaderboard = structuredClone(mockProblemDataLeaderboard);
+        noProblemInstancesMockProblemDataLeaderboard.benchmark_instances = [];
+        
+        // Render the AggregateLeaderBoard component wrapped in BrowserRouter
+        renderWithRouter(true, () => (
+            <ProblemOccurrenceProblemInstanceList 
+                problemData={noProblemInstancesMockProblemDataLeaderboard}
+                leaderboardData={[]}
+            />)
+        );
+
+        // Wait for the useEffect and fetchData to complete
+        await waitFor(async () => {
+            // Check whether the correct text is displayed
+            expect(screen.getAllByText('No entries found')[0]).toBeVisible(); //the getallby[0] is because the instanceleaderboard also displays 'no entries found'
+        });
+    });
+    
     it('succesfull render', async () => {
         renderWithRouter(true, () => (
             <ProblemOccurrenceProblemInstanceList 
@@ -14,15 +46,30 @@ describe('Problem occurrence instance list', () => {
                 leaderboardData={mockLeaderboardData}
             />
         ));
-
-        // TODO: Chance visibility requirement
-        expect(screen.getByText(mockProblemDataLeaderboard.benchmark_instances[0].name)).toBeVisible();
-        // expect(screen.queryByText(mockProblemDataLeaderboard.benchmark_instances[1].id)).not.toBeVisible();
-
-        // Simulate changing the select value
-        fireEvent.change(screen.getByRole('combobox'), { target: { value: '1' } });
-        expect(screen.getByText(mockProblemDataLeaderboard.benchmark_instances[1].name)).toBeVisible();
-        // expect(screen.queryByText(mockProblemDataLeaderboard.benchmark_instances[0].id)).not.toBeVisible();
+        
+        // TODO: when the intance names are merged into main, change this test to name instead of id
+        for (let instance of mockProblemDataLeaderboard.benchmark_instances) {
+            expect(screen.getByText(instance.name)).toBeVisible();
+        }
     });
-
+    
+    it('row folds open', async () => {
+        renderWithRouter(true, () => (
+            <ProblemOccurrenceProblemInstanceList 
+                problemData={mockProblemDataLeaderboard}
+                leaderboardData={mockLeaderboardData}
+            />
+        ));
+        
+        await waitFor(async () => {
+            const row = screen.getByText(mockProblemDataLeaderboard.benchmark_instances[0].name).closest("td").closest("tr");
+            const hiddenLeaderboard = screen.getAllByText("The score")[0].closest("table").closest("div").closest("td").closest("tr");
+            
+            //the instance leaderboard should be collapsed before clicking the row
+            expect(hiddenLeaderboard).not.toBeVisible();
+            
+            await userEvent.click(row);
+            expect(hiddenLeaderboard).toBeVisible();
+        });
+    });
 }); 
