544d74ba42085782a298a904e692fc8c7b763949
Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>
2024-07-03 11:33:22 +0200

    more coverage

--- backend/tests/test_leaderboard_entry.py
+++ backend/tests/test_leaderboard_entry.py
@@ -1,58 +1,47 @@
 import json
+import random
 
 from api.data_processing.leaderboard_entry import LeaderboardEntry
 from api.views.leaderboard_entry_view import LeaderboardEntryView
+from api.models import Result, Submission
 
 from .create_test_data import CreateTestData
 
 
-class LeaderboardEntryViewTest(CreateTestData):
+class LeaderboardEntryTest(CreateTestData):
     def setUp(self):
-        super(LeaderboardEntryViewTest, self).setUp()
+        super(LeaderboardEntryTest, self).setUp()
 
-        # Create mock view
-        self.view = LeaderboardEntryView().as_view()
-
-        self.problem.benchmark_instances.set([self.benchmark])
-
+    # Test for valid creation of leaderboard entry
+    def test_leaderboard_entry_valid(self):
         # Create LeaderboardEntry for testing purposes
-        self.entries = LeaderboardEntry(self.problem, self.submission)
+        self.leaderboard_entry = LeaderboardEntry(self.problem, self.submission)
 
-    def test_leaderboard_entry(self):
-        # Create mock request
-        self.req = self.rf.get(f'/api/leaderboard_entry/{str(self.problem.id)}/{str(self.submission.id)}')
-        response = self.view(self.req, self.problem.id, self.submission.id)
+        # Assert the properties of the leaderboard
+        self.assertEqual(self.leaderboard_entry.submitter.name, self.test_user.name)
+        self.assertEqual(self.leaderboard_entry.submission, self.submission)
+        self.assertEqual(self.leaderboard_entry.instance_entries[0].benchmark_instance.filepath, self.benchmark.filepath)
+        self.assertEqual(self.leaderboard_entry.results[self.sc_metric.name], self.result.score)
 
-        # Unpack the json object
-        result = json.loads(response.content.decode())
+    # Test for valid creation of leaderboard entry
+    def test_leaderboard_entry_non_valid(self):
+        # Create LeaderboardEntry with no results
+        self.result = None
+        Result.objects.all().delete()
+        self.submission.is_verified = True
+        self.leaderboard_entry = LeaderboardEntry(self.problem, self.submission)
 
-        # Check reponses
-        self.assertEqual(response.status_code, 200)
+        # Assert the properties of the leaderboard
+        self.assertEqual(self.leaderboard_entry.submitter.name, self.test_user.name)
+        self.assertEqual(self.leaderboard_entry.submission, self.submission)
+        self.assertEqual(self.leaderboard_entry.instance_entries[0].benchmark_instance.filepath, self.benchmark.filepath)
+        self.assertEqual(self.leaderboard_entry.results, dict())
 
-        # Test attributes for submission
-        self.assertEqual(str(self.submission.id), result['submission']['id'])
-        self.assertEqual(str(self.problem.id), result['submission']['problem'])
-        self.assertEqual(self.submission.name, result['submission']['name'])
-        
-        # Test attributes for results object
-        self.assertEqual(self.result.score, float(result['instance_entries'][0]["results"]['TestMetric']))
-        
-    def test_leaderboard_entry_bad_submission(self):
-        # Create mock request
-        self.req = self.rf.get(f'/api/leaderboard_entry/{str(self.problem.id)}/f9e9cb40-9f4c-4fa3-8fa5-2004f4e02111')
-        response = self.view(self.req, self.problem.id, 'f9e9cb40-9f4c-4fa3-8fa5-2004f4e02111')
+    # Test for valid creation of leaderboard entry
+    def test_leaderboard_entry_competition(self):
+        # Create LeaderboardEntry with competition style category
+        self.category.style = 0
+        self.leaderboard_entry = LeaderboardEntry(self.problem, self.submission)
 
-        # Assert the response status code
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.data,  {'error':"Submission with id f9e9cb40-9f4c-4fa3-8fa5-2004f4e02111 was not found"}, 'submission id wrong"')
-        
-
-    def test_leaderboard_entry_bad_problem(self):
-        # Create mock request
-        self.req = self.rf.get(f'/api/leaderboard/f9e9cb40-9f4c-4fa3-8fa5-2004f4e02111/{str(self.submission.id)}')
-        response = self.view(self.req, "f9e9cb40-9f4c-4fa3-8fa5-2004f4e02111",  self.submission.id)
-
-        # Assert the response status code
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.data,  {'error':"Problem with id f9e9cb40-9f4c-4fa3-8fa5-2004f4e02111 was not found"}, 'submission id wrong"')
-        
+        # Assert the properties of the leaderboard
+        self.assertEqual(self.leaderboard_entry.instance_entries, [])
---
+++ backend/tests/test_leaderboard_instance_entry.py
@@ -0,0 +1,44 @@
+import json
+import random
+
+from api.data_processing.leaderboard_instance_entry import LeaderboardInstanceEntry
+from api.models import Result, Submission
+
+from .create_test_data import CreateTestData
+
+
+class LeaderboardInstanceEntryTest(CreateTestData):
+    def setUp(self):
+        super(LeaderboardInstanceEntryTest, self).setUp()
+
+    # Test for valid creation of leaderboard entry
+    def test_leaderboard_instance_entry_valid(self):
+        # Verify submission
+        self.submission.is_verified = True
+
+        # Create a duplicate result for that submission
+        self.result2 = Result.objects.create(
+            metric=self.sc_metric,
+            # Randomly generate value
+            score=self.result.score,
+            benchmark_instance=self.benchmark,
+            submission=self.submission
+        )
+
+        # Create LeaderboardEntry for testing purposes
+        self.leaderboard_instance_entry = LeaderboardInstanceEntry(self.submission, self.benchmark)
+
+        # Assert the properties of the leaderboard
+        self.assertEqual(self.leaderboard_instance_entry.benchmark_instance, self.benchmark)
+        self.assertEqual(self.leaderboard_instance_entry.results[self.sc_metric.name], self.result.score)
+
+    # Test for valid creation of leaderboard entry
+    def test_leaderboard_instance_entry_non_valid(self):
+        # Create LeaderboardEntry with no results
+        self.result = None
+        Result.objects.all().delete()
+        self.submission.is_verified = True
+        self.leaderboard_instance_entry = LeaderboardInstanceEntry(self.submission, self.benchmark)
+
+        # Assert the properties of the leaderboard
+        self.assertEqual(self.leaderboard_instance_entry.results, dict())
